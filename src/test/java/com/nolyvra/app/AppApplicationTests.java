ff9ab718602c27ee2a9f4615c8cc68569322b265
Ethan XYJ <endorathas@gmail.com>
2026-05-02 18:35:33 +1000

    chore: consolidate Spring Boot application config

--- src/test/java/com/nolyvra/app/AppApplicationTests.java
+++ src/test/java/com/nolyvra/app/AppApplicationTests.java
@@ -3,7 +3,7 @@ package com.nolyvra.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "nolyvra.mock-ai=true")
 class AppApplicationTests {
 
 	@Test
